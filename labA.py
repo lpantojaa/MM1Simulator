426a80dccf77debddf1147379e1763d535c9f94c
Leandro Pantoja <lmpantoja@hotmail.com>
2023-06-05 13:42:23 +1000

    Poisson source generator

--- labA.py
+++ labA.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 #Class to abstracty the packet structure
 class packet:
     def _init_(self, id, arrival_time, departure_time, size):
@@ -8,3 +11,10 @@ class packet:
     
     def __lt__(self, other):
         return self.arrival_time < other.arrival_time
+
+#Class that generates exponential inter-arrival times and lengths
+class poisson_process_source:
+    def generate_packet_information(self):
+        #Exponential inter-arrival times imply Poisson distributed arrivals
+        self.inter_arrival = np.random.exponential(1/self.lambd, 1)
+        self.packet_size = math.ceil(np.random.exponential(10000))
